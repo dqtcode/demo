dcfc1ab4dad7c4e4ff3d80874478d7c4d024aced
vagrant <vagrant@homestead>
2020-03-08 05:34:43 +0000

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>@yield('title','微博首页')</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','关于')
+@section('content')
+<h2>关于</h2>
+@stop
