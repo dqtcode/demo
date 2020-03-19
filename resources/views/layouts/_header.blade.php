ac4a1fd3b1699a42ed8b736a404df8eb44971408
vagrant <vagrant@homestead>
2020-03-19 06:38:24 +0000

    用户脱出登录

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -20,13 +20,11 @@
 						
 					</div>
 					<a href="#" class="dropdown-item" id="logout">
-						<form action="{{route('logout')}}" method="POST">
-						{{csrf_field()}}
-						{{method_field('DELETE')}}
-						<button class="btn btn-block btn-danger" type="submit" name="button">
-							退出
-						</button>
-						</form>
+<form action="{{ route('logout') }}" method="POST">
+  {{ csrf_field() }}
+  {{ method_field('DELETE') }}
+  <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+</form>
 					</a>
 				</div>
 			</li>
